d38e1fe2fe10ce5df09a4cae053e0903063f182d
sanajylopis25 <sanajylopis25@gmail.com>
2022-01-14 23:30:05 +0530

    First Commit

---
+++ FileIO/SerializationDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SerializationDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //BinarySerialization obj = new BinarySerialization();
+            //obj.Serialization();
+            BinaryDeSerialization obj1 = new BinaryDeSerialization();
+            obj1.DeSerialization();
+        }
+    }
+}
